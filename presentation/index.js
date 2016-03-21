ff8a9310689479dc6cf094c5828ff77f093d58b2
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-03-20 23:02:39 -0400

    first presentation commit

--- presentation/index.js
+++ presentation/index.js
@@ -1,172 +1,189 @@
 // Import React
-import React from "react";
+import React from 'react';
+import * as _ from 'lodash';
 
 // Import Spectacle Core tags
 import {
-  Appear,
-  BlockQuote,
-  Cite,
-  CodePane,
-  Deck,
-  Fill,
-  Heading,
-  Image,
-  Layout,
-  Link,
-  ListItem,
-  List,
-  Markdown,
-  Quote,
-  Slide,
-  Spectacle,
-  Text
-} from "spectacle";
+    Code,
+    CodePane,
+    Deck,
+    Fill,
+    Heading,
+    Image,
+    Layout,
+    Link,
+    ListItem,
+    List,
+    Slide,
+    Spectacle,
+    Text
+} from 'spectacle';
 
 // Import image preloader util
-import preloader from "spectacle/lib/utils/preloader";
+import preloader from 'spectacle/lib/utils/preloader';
 
 // Import theme
-import createTheme from "spectacle/lib/themes/default";
+import createTheme from 'spectacle/lib/themes/default';
 
 // Import custom component
-import Interactive from "../assets/interactive";
+// import Interactive from '../assets/interactive';
 
 // Require CSS
-require("normalize.css");
-require("spectacle/lib/themes/default/index.css");
+require('normalize.css');
+require('spectacle/lib/themes/default/index.css');
 
 
 const images = {
-  city: require("../assets/city.jpg"),
-  kat: require("../assets/kat.png"),
-  logo: require("../assets/formidable-logo.svg"),
-  markdown: require("../assets/markdown.png")
+    react: require('../assets/react-logo-white.svg')
 };
 
 preloader(images);
 
-const theme = createTheme({
-  primary: "#ff4081"
-});
+const theme = createTheme(
+    {
+        primary: '#1976D2' //'#00d8ff'
+    },
+    {
+        primary: 'Roboto, sans-serif'
+    }
+);
 
 export default class Presentation extends React.Component {
-  render() {
-    return (
-      <Spectacle theme={theme}>
-        <Deck transition={["zoom", "slide"]} transitionDuration={500}>
-          <Slide transition={["zoom"]} bgColor="primary">
-            <Heading size={1} fit caps lineHeight={1} textColor="black">
-              Spectacle
-            </Heading>
-            <Heading size={1} fit caps>
-              A ReactJS Presentation Library
-            </Heading>
-            <Heading size={1} fit caps textColor="black">
-              Where You Can Write Your Decks In JSX
-            </Heading>
-            <Link href="https://github.com/FormidableLabs/spectacle">
-              <Text bold caps textColor="tertiary">View on Github</Text>
-            </Link>
-            <Text textSize="1.5em" margin="20px 0px 0px" bold>Hit Your Right Arrow To Begin!</Text>
-          </Slide>
-          <Slide transition={["slide"]} bgColor="black" notes="You can even put notes on your slide. How awesome is that?">
-            <Image src={images.kat.replace("/", "")} margin="0px auto 40px" height="293px"/>
-            <Heading size={2} caps fit textColor="primary" textFont="primary">
-              Wait what?
-            </Heading>
-          </Slide>
-          <Slide transition={["zoom", "fade"]} bgColor="primary" notes="<ul><li>talk about that</li><li>and that</li></ul>">
-            <CodePane
-              lang="jsx"
-              source={require("raw!../assets/deck.example")}
-              margin="20px auto"
-            />
-          </Slide>
-          <Slide transition={["slide"]} bgImage={images.city.replace("/", "")} bgDarken={0.75}>
-            <Appear fid="1">
-              <Heading size={1} caps fit textColor="primary">
-                Full Width
-              </Heading>
-            </Appear>
-            <Appear fid="2">
-              <Heading size={1} caps fit textColor="tertiary">
-                Adjustable Darkness
-              </Heading>
-            </Appear>
-            <Appear fid="3">
-              <Heading size={1} caps fit textColor="primary">
-                Background Imagery
-              </Heading>
-            </Appear>
-          </Slide>
-          <Slide transition={["zoom", "fade"]} bgColor="primary">
-            <Heading caps fit>Flexible Layouts</Heading>
-            <Layout>
-              <Fill>
-                <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                  Left
-                </Heading>
-              </Fill>
-              <Fill>
-                <Heading size={4} caps textColor="secondary" bgColor="white" margin={10}>
-                  Right
-                </Heading>
-              </Fill>
-            </Layout>
-          </Slide>
-          <Slide transition={["slide"]} bgColor="black">
-            <BlockQuote>
-              <Quote>Wonderfully formatted quotes</Quote>
-              <Cite>Ken Wheeler</Cite>
-            </BlockQuote>
-          </Slide>
-          <Slide transition={["spin", "zoom"]} bgColor="tertiary">
-            <Heading caps fit size={1} textColor="primary">
-              Inline Markdown
-            </Heading>
-            <Markdown>
-              {`
-![Markdown Logo](${images.markdown.replace("/", "")})
-
-You can write inline images, [Markdown Links](http://commonmark.org), paragraph text and most other markdown syntax
-* Lists too!
-* With ~~strikethrough~~ and _italic_
-* And lets not forget **bold**
-              `}
-            </Markdown>
-          </Slide>
-          <Slide transition={["slide", "spin"]} bgColor="primary">
-            <Heading caps fit size={1} textColor="tertiary">
-              Smooth
-            </Heading>
-            <Heading caps fit size={1} textColor="secondary">
-              Combinable Transitions
-            </Heading>
-          </Slide>
-          <Slide transition={["fade"]} bgColor="secondary" textColor="primary">
-            <List>
-              <Appear><ListItem>Inline style based theme system</ListItem></Appear>
-              <Appear><ListItem>Autofit text</ListItem></Appear>
-              <Appear><ListItem>Flexbox layout system</ListItem></Appear>
-              <Appear><ListItem>React-Router navigation</ListItem></Appear>
-              <Appear><ListItem>PDF export</ListItem></Appear>
-              <Appear><ListItem>And...</ListItem></Appear>
-            </List>
-          </Slide>
-          <Slide transition={["slide"]} bgColor="primary">
-            <Heading size={1} caps fit textColor="tertiary">
-              Your presentations are interactive
-            </Heading>
-            <Interactive/>
-          </Slide>
-          <Slide transition={["spin", "slide"]} bgColor="tertiary">
-            <Heading size={1} caps fit lineHeight={1.5} textColor="primary">
-              Made with love in Seattle by
-            </Heading>
-            <Link href="http://www.formidablelabs.com"><Image width="100%" src={images.logo}/></Link>
-          </Slide>
-        </Deck>
-      </Spectacle>
-    );
-  }
+    render() {
+        return (
+            <Spectacle
+                theme={theme}>
+                <Deck
+                    transition={['slide']}
+                    transitionDuration={500}
+                    progress="pacman"
+                    controls={true}>
+                    <Slide align="center center" notes='React!' id={_.uniqueId('slide_')}>
+                        <Image src={images.react} height="200px"/>
+                        <Heading size={2} textColor="white">
+                            Introduction to React
+                        </Heading>
+                        <Text textColor="white" margin="16px" textSize="24px">
+                            @yashdalfthegray
+                        </Text>
+                    </Slide>
+                    <Slide align="center center" id={_.uniqueId('slide_')}>
+                        <Text textColor="white">
+                            A library for building user interfaces.
+                        </Text>
+                    </Slide>
+                    <Slide align="center center" id={_.uniqueId('slide_')}>
+                        <Heading size={4} textColor="white">
+                            Basic Syntax
+                        </Heading>
+                        <CodePane
+                            margin="32px"
+                            lang="javascript"
+                            source={require("raw!../assets/basic-syntax.example")} />
+                    </Slide>
+                    <Slide align="center center" id={_.uniqueId('slide_')}>
+                        <Heading size={4} textColor="white" margin="32px">
+                            JSX
+                        </Heading>
+                        <Layout>
+                            <Fill>
+                                <CodePane
+                                    lang="jsx"
+                                    source={require("raw!../assets/jsx-es5.example")} />
+                            </Fill>
+                            <Fill>
+                                <CodePane
+                                    lang="jsx"
+                                    source={require("raw!../assets/jsx-es6.example")} />
+                            </Fill>
+                        </Layout>
+                    </Slide>
+                    <Slide align="center center" id={_.uniqueId('slide_')}>
+                        <Text textColor="white">
+                            It's components all the way down.
+                        </Text>
+                    </Slide>
+                    <Slide align="center center" notes='' id={_.uniqueId('slide_')}>
+                        <Heading size={4} textColor="white">
+                            Data Flow
+                        </Heading>
+                        <List margin="32px">
+                            <ListItem textColor="white"><Code textColor="white">this.props</Code></ListItem>
+                            <ListItem textColor="white">Event Handlers</ListItem>
+                        </List>
+                    </Slide>
+                    <Slide align="center center" notes='' id={_.uniqueId('slide_')}>
+                        <Heading size={4} textColor="white">
+                            Component State
+                        </Heading>
+                        <List margin="32px">
+                            <ListItem textColor="white"><Code textColor="white">this.state</Code></ListItem>
+                            <ListItem textColor="white"><Code textColor="white">this.setState()</Code></ListItem>
+                        </List>
+                    </Slide>
+                    <Slide align="center center" id={_.uniqueId('slide_')}>
+                        <Heading size={4} textColor="white" margin="32px">
+                            Performance
+                        </Heading>
+                        <Text textColor="white" margin="32px">
+                            Browser DOM
+                        </Text>
+                        <Text textColor="white" margin="32px">
+                            Optimizations?
+                        </Text>
+                    </Slide>
+                    <Slide align="center center" notes='' id={_.uniqueId('slide_')}>
+                        <Heading size={4} textColor="white">
+                            Virtual DOM
+                        </Heading>
+                        <Text textColor="white" margin="32px">
+                            Diffin' all over the world!
+                        </Text>
+                    </Slide>
+                    <Slide align="center center" notes='' id={_.uniqueId('slide_')}>
+                        <Heading size={4} textColor="white">
+                            More Resources
+                        </Heading>
+                        <List margin="32px">
+                            <ListItem textColor="white">
+                                <Link href="">
+                                    <Text textColor="white">Outline</Text>
+                                </Link>
+                            </ListItem>
+                            <ListItem textColor="white">
+                                <Link href="https://facebook.github.io/react/docs/tutorial.html">
+                                    <Text textColor="white">React Tutorial</Text>
+                                </Link>
+                            </ListItem>
+                            <ListItem textColor="white">
+                                <Link href="https://github.com/kohei-takata/learnyoureact">
+                                    <Text textColor="white">Thinking in React</Text>
+                                </Link>
+                            </ListItem>
+                            <ListItem textColor="white">
+                                <Link href="https://github.com/asbjornenge/thinking-in-react">
+                                    <Text textColor="white">Learn You React</Text>
+                                </Link>
+                            </ListItem>
+                            <ListItem textColor="white">
+                                <Link href="https://facebook.github.io/flux/docs/overview.html#content">
+                                    <Text textColor="white">Flux</Text>
+                                </Link>
+                            </ListItem>
+                            <ListItem textColor="white">
+                                <Link href="http://redux.js.org/">
+                                    <Text textColor="white">Redux</Text>
+                                </Link>
+                            </ListItem>
+                        </List>
+                    </Slide>
+                    <Slide align="center center" notes='' id={_.uniqueId('slide_')}>
+                        <Heading size={4} textColor="white">
+                            Thank you!
+                        </Heading>
+                    </Slide>
+                </Deck>
+            </Spectacle>
+        );
+    }
 }
